2cc7ce7aa67ea6ba4d5c27df8adc0986dd03892e
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-11-28 15:23:38 +0100

    Update install.sh
    The install script now force overwrites old files

--- install.sh
+++ install.sh
@@ -13,18 +13,18 @@ echo "Cloning repo..."
 git clone  https://github.com/RobinBoers/FramboxOS /home/pi/Frambox
 
 echo "Copying files..."
-sudo cp /home/pi/Frambox/.bashrc /home/pi/.bashrc
-sudo cp /home/pi/Frambox/splashscreen.list /etc/splashscreen.list
+sudo cp -f /home/pi/Frambox/.bashrc /home/pi/.bashrc
+sudo cp -f /home/pi/Frambox/splashscreen.list /etc/splashscreen.list
 sudo cp -f /home/pi/Frambox/startes.sh /home/pi/startes.sh
 
 echo "Setting up static IP..."
-sudo cp /home/pi/Frambox/dhcpcd.conf /etc/dhcpcd.conf
+sudo cp -f /home/pi/Frambox/dhcpcd.conf /etc/dhcpcd.conf
 
 echo "Installing gamelists and ports..."
-sudo cp -R /home/pi/Frambox/roms/ /home/pi/RetroPie/roms/
-sudo cp -R /home/pi/Frambox/gamelists/ /opt/retropie/configs/all/emulationstation/gamelists/
+sudo cp -R -f /home/pi/Frambox/roms/ /home/pi/RetroPie/roms/
+sudo cp -R -f /home/pi/Frambox/gamelists/ /opt/retropie/configs/all/emulationstation/gamelists/
 
 echo "Setting up Raspbian menu..."
-sudo cp -R /home/pi/Frambox/applications /usr/share/raspi-ui-overrides/applications
+sudo cp -R -f /home/pi/Frambox/applications /usr/share/raspi-ui-overrides/applications
 
 echo "Done. Please reboot now"
