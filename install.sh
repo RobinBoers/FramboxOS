2820a19f2ece3341566361c12a104cd8ff03aea4
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-11-28 15:11:23 +0100

    Added menu config
    Added override files for Raspbian menu. This shows some hidden personalisation options, and some hidden programs.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Installing requierd packages..."
+sudo apt-get install -y python3 idle3
+sudo apt-get install -y python3-pip
 sudo apt-get install -y minecraft-pi
 sudo apt-get install -y hping3
 sudo apt-get install -y nmap
@@ -20,5 +22,8 @@ sudo cp /home/pi/Frambox/dhcpcd.conf /etc/dhcpcd.conf
 echo "Installing gamelists and ports..."
 sudo cp -R /home/pi/Frambox/roms/ /home/pi/RetroPie/roms/
 sudo cp -R /home/pi/Frambox/gamelists/ /opt/retropie/configs/all/emulationstation/gamelists/
+
+echo "Setting up Raspbian menu..."
+sudo cp -R /home/pi/Frambox/applications /usr/share/raspi-ui-overrides/applications
 
 echo "Done. Please reboot now"
